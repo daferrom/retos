63a204635fd7c32f199fa9db32d2ef1e00dec182
daferrom <daferrom@gmail.com>
2021-08-17 22:59:32 -0500

    final version

--- 08Hoverboard/script.js
+++ 08Hoverboard/script.js
@@ -1,29 +1,37 @@
-const container = document.getElementById('')
-const colors = ['', '', '', '', '']
+const container = document.getElementById('container')
+const colors = ['#00ffff', '#FF00FF', '#FFFF00', '#00FF00', '#ff0000','#0000FF']
 const SQUARES = 500
-
-for(let i = 0; i < SQUARES; ) {
-    const square = document.createElement('')
-    square.classList.add('')
-
-    square.addEventListener('', () => setColor(square))
-
-    square.addEventListener('', () => removeColor(square))
+/////Creacion de cuadrados con for///
+for(let i = 0; i < SQUARES;i++ ) {
+    const square = document.createElement('div')
+////Añade la clase 'square' al div creado cargando las propiedades de la clase desde CSS//
+    square.classList.add('square')
+////llama ala función set color cuando al elemento square cuando (el evento) el mouse por encima /// 
+     square.addEventListener('mouseover', () => setColor(square))
+////llama ala función remove color cuando al elemento square cuando (el evento) el mouse por encima ///
+    square.addEventListener('mouseout', () => removeColor(square))
 
     container.appendChild(square)
 }
 
+////// la funcion escoge un Background-color al azar para el elemento square//////////
 function setColor(element) {
-   const color = getRandomColor()
-   element.style.background = 
-   element.style.boxShadow = ``
+   const color = getRandomColor();
+   element.style.background = color;
+/// crea una sombra "resplandor del cuadrado" cuando evento se dispara
+    element.style.boxShadow = `0 0 2px ${color},0 0 10px ${color}`
 }
 
 function removeColor(element) {
-   element.style.background = '#'
-   element.style.boxShadow = ''
+   element.style.background = '#1d1d1d'
+   /// retira la sombra "resplandor del cuadrado" cuando evento se dispara
+   element.style.boxShadow = '0 0 2px #000'
 }
 
+////// la funcion escoge un Background-color al azar para el elemento square//////////
 function getRandomColor() {
-    return colors[Math.floor()]
+/// Math.floor redondea un numero dado al entero menor//
+/// Math.random escoge un numero entre [0,1) al azar///
+/// el return devuelve del array colors , un color elegido al azar del arreglo///
+return colors[Math.floor(Math.random() * colors.length)]
 }
