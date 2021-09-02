000d9c0e7812b0b045216e781d18f12f3d13a3f9
daferrom <daferrom@gmail.com>
2021-09-01 23:23:59 -0500

    challenge 10 completed

--- 10-Form input wave/script.js
+++ 10-Form input wave/script.js
@@ -1,8 +1,11 @@
+// it takes the tags "label" and save it in a variable" //
 const labels = document.querySelectorAll('.form-control label')
-
+console.log(labels)
+// the function walks each elemnt label //  
 labels.forEach(label => {
+// the function walks each elemnt label //  
     label.innerHTML = label.innerText
-        .split('')
-        .map((letter, idx) => `<span>${letter}</span>`)
-        .join('')
+    .split('')
+    .map((letter, index) => `<span style ="transition-delay:${index*50}ms">${letter}</span>`)
+    .join('');
 })
