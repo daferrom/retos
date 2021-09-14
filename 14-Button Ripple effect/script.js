e70e35001d5ddbee1efaae006a269f2f37e1238e
daferrom <daferrom@gmail.com>
2021-09-14 00:06:22 -0500

    challenge 14 almost complete

---
+++ 14-Button Ripple effect/script.js
@@ -0,0 +1,35 @@
+
+
+const buttons = document.querySelectorAll('.button')
+
+buttons.forEach(button => {
+    button.addEventListener('click', function (e) {
+        
+        let x = e.clientX - e.target.offsetLeft;
+        let y = e.clientY - e.target.offsetTop;
+
+        //revisar 
+        console.log(x,y);
+
+        const buttonTop = e.target.offsetTop - y
+        const buttonLeft = e.target.offsetLeft - x
+
+        console.log(buttonTop,buttonLeft);
+
+        /*let xInside = x - buttonLeft
+        let yInside = y - buttonTop
+*/
+        //console.log(xInside,yInside);
+
+        let ripples = document.createElement('span');
+        ripples.style.left = x +'px'
+        ripples.style.top = y +'px'
+        this.appendChild(ripples)
+        
+        setTimeout(() => {ripples.remove()},1000);
+        
+        
+    })
+})
+
+var name ="john"
