93d406f6f7190d996a92d8a48de7ddab26de8d6a
daferrom <daferrom@gmail.com>
2021-09-14 00:15:25 -0500

    challenge 14 almost completed 2.0

--- 14-Button Ripple effect/script.js
+++ 14-Button Ripple effect/script.js
@@ -5,20 +5,20 @@ const buttons = document.querySelectorAll('.button')
 buttons.forEach(button => {
     button.addEventListener('click', function (e) {
         
-        let x = e.clientX - e.target.offsetLeft;
-        let y = e.clientY - e.target.offsetTop;
+        let x = e.clientX - e.target.offsetLeft -60;
+        let y = e.clientY - e.target.offsetTop -20;
 
         //revisar 
         console.log(x,y);
 
-        const buttonTop = e.target.offsetTop - y
-        const buttonLeft = e.target.offsetLeft - x
+        const buttonTop = e.target.offsetTop 
+        const buttonLeft = e.target.offsetLeft 
 
         console.log(buttonTop,buttonLeft);
 
-        /*let xInside = x - buttonLeft
+        let xInside = x - buttonLeft
         let yInside = y - buttonTop
-*/
+
         //console.log(xInside,yInside);
 
         let ripples = document.createElement('span');
