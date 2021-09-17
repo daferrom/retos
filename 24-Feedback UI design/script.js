d72ddc2ec8f85d56bf9e4fb4770da58a3b495b5b
daferrom <daferrom@gmail.com>
2021-09-16 23:17:40 -0500

    feed back ui design completed

--- 24-Feedback UI design/script.js
+++ 24-Feedback UI design/script.js
@@ -1,7 +1,7 @@
-const ratings = document.querySelectorAll('.raing')
-const ratingsContainer = document.querySelector('.-container')
-const sendBtn = document.querySelector('#')
-const panel = document.querySelector('#')
+const ratings = document.querySelectorAll('.rating')
+const ratingsContainer = document.querySelector('.ratings-container')
+const sendBtn = document.querySelector('#send')
+const panel = document.querySelector('#panel')
 
 
 ratingsContainer.addEventListener('click', (e) => {
@@ -10,16 +10,25 @@ ratingsContainer.addEventListener('click', (e) => {
         e.target.parentNode.classList.add('active')
         selectedRating = e.target.nextElementSibling.innerHTML
     }
+    if(e.target.classList.contains('rating')) {
+        removeActive()
+        e.target.classList.add('active')
+        selectedRating = e.target.nextElementSibling.innerHTML
+    }
+
 })
-
-sendBtn.addEventListener('', (e) => {
+sendBtn.addEventListener('click', (e) => {
     panel.innerHTML = `
-
+        <i class="fas fa-heart"></i>
+        <strong>Thank You!</strong>
+        <br>
+        <strong>Feedback: ${selectedRating}</strong>
+        <p>We'll use your feedback to improve our customer support</p>
     `
 })
 
 function removeActive() {
-{
+    for(let i = 0; i < ratings.length; i++) {
         ratings[i].classList.remove('active')
     }
 }
